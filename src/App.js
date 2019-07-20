49de500ecd70f1f3088e6a67f7cda7dbccb5ff02
samuraikun <tazitaziawawa@gmail.com>
2019-07-20 14:35:32 +0900

    connetc React and AppSync

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import API, { graphqlOperation } from '@aws-amplify/api'
+import PubSub from '@aws-amplify/pubsub'
+import { createTodo } from './graphql/mutations'
+import { listTodos } from './graphql/queries'
+import { onCreateTodo } from './graphql/subscriptions'
+
+import config from './aws-exports'
+API.configure(config)
+PubSub.configure(config)
+
+async function createNewTodo() {
+  const todo = { name: "Use AppSync", description: "Realtime and Offline" }
+  await API.graphql(graphqlOperation(createTodo, { input: todo }))
 }
 
-export default App;
+const initialState = { todos: [] }
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'QUERY':
+      return {...state, todos: action.todos }
+    case 'SUBSCRIPTION':
+      return {...state, todos: [...state.todos, action.todo] }
+    default:
+      return state
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    getData()
+
+    const subscription = API.graphql(graphqlOperation(onCreateTodo)).subscribe({
+      next: eventData => {
+        const todo = eventData.value.data.onCreateTodo
+        dispatch({ type: 'SUBSCRIPTION', todo })
+      }
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  async function getData() {
+    const todoData = await API.graphql(graphqlOperation(listTodos))
+    const payload = { type: 'QUERY', todos: todoData.data.listTodos.items}
+
+    dispatch(payload)
+  }
+
+  return (
+    <>
+      <div className="App">
+        <button onClick={createNewTodo}>Add Todo</button>
+      </div>
+      <div>{ state.todos.map((todo, i) => <p key={todo.id}>{todo.name} : {todo.description}</p>)}</div>
+    </>
+  )
+}
+
+export default App
